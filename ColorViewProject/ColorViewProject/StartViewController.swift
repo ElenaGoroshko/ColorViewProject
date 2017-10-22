a7bc617450909fafec4a4e6afece83823761c5a2
ElenaGoroshko <32428178+ElenaGoroshko@users.noreply.github.com>
2017-10-22 13:36:59 +0300

    add 2 windows

---
+++ ColorViewProject/ColorViewProject/StartViewController.swift
@@ -0,0 +1,20 @@
+//
+//  StartViewController.swift
+//  ColorViewProject
+//
+//  Created by Elena Goroshko on 10/22/17.
+//  Copyright © 2017 Elena Goroshko. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+ 
+}
